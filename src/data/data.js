0bdb23790f7121d5c12126912101a10d2ac25834
dannertech <danner@danner.tech>
2023-04-10 18:46:37 -0400

    added sample data

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        "category": "Games",
+        "titles": ["Space Race", "Mom Fight", "Marble Maze"]
+    },
+    {
+        "category": "Drinks",
+        "titles": ["Long Island", "Vodka Cranberry", "Margarita", "Lemon Drop"]
+    },
+    {
+        "category": "Food",
+        "titles": ["Parmesan Wings", "Shrimp Fried Rice", "Philly Cheesesteak Eggrolls"]
+    },
+    {
+        "category": "Desserts",
+        "titles": ["Cheesecake rolls", "Chocolate Lava cake", "Funnel fries"]
+    },
+    {
+        "category": "Merchandise",
+        "titles": ["T Shirt"]
+    }
+];
