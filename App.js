94a805f5721586f8ad7d403c0db6a1d7d4619fee
dannertech <danner@danner.tech>
2023-04-10 19:18:16 -0400

    edited data, added FlatList to App.js

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import {FlatList, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, View, Text, StyleSheet} from 'react-native';
+import { data } from './src/data/data';
 
 const App = () => {
+  const [title, setTitle] = useState('');
   return (
-    <View>
-      <Text>This is the App screen</Text>
+    <View style={styles.mainView}>
+      <FlatList
+        data={data}
+        keyExtractor={((item) => item.category)}
+        renderItem={(item) => {
+          return (
+            <View style={styles.cardStyle}>
+            </View>
+          )
+        }}
+      />
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1
+  },
+  cardStyle: {
+    flexGrow: 1
+  },
+  subtitleView: {
+    flexGrow: 1
+  }
+});
 
 export default App;
--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,27 @@
 export const data = [
     {
         "category": "Games",
-        "titles": ["Space Race", "Mom Fight", "Marble Maze"]
+        "titles": ["Space Race", "Mom Fight", "Marble Maze"],
+        "color": "red"
     },
     {
         "category": "Drinks",
-        "titles": ["Long Island", "Vodka Cranberry", "Margarita", "Lemon Drop"]
+        "titles": ["Long Island", "Vodka Cranberry", "Margarita", "Lemon Drop"],
+        "color": "purple"
     },
     {
         "category": "Food",
-        "titles": ["Parmesan Wings", "Shrimp Fried Rice", "Philly Cheesesteak Eggrolls"]
+        "titles": ["Parmesan Wings", "Shrimp Fried Rice", "Philly Cheesesteak Eggrolls"],
+        "color": "yellow"
     },
     {
         "category": "Desserts",
-        "titles": ["Cheesecake rolls", "Chocolate Lava cake", "Funnel fries"]
+        "titles": ["Cheesecake rolls", "Chocolate Lava cake", "Funnel fries"],
+        "color": "green"
     },
     {
         "category": "Merchandise",
-        "titles": ["T Shirt"]
+        "titles": ["T Shirt"],
+        "color": "orange"
     }
 ];
